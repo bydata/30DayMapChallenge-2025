3d5dba0c0c229d0b94ce643855e2eed38336a042
bydata <ansgar.wolsing@gmail.com>
2025-11-27 23:46:00 +0100

    parse multiple files

--- R/26-transport/parse-xml-nextbike.R
+++ R/26-transport/parse-xml-nextbike.R
@@ -1,53 +1,57 @@
+library(tidyverse)
 library(xml2)
-library(dplyr)
 library(here)
 
-xml_content <- here("data", "26-transport", "data_20251126_000739.xml")
-xml_doc <- read_xml(xml_content)
+# Read files
+input_path <- here("data", "26-transport")
+xml_files <- list.files(here(input_path), pattern = "data_.*\\.xml")
+xml_docs <- map(here(input_path, xml_files), read_xml)
+xml_docs <- set_names(xml_docs, str_extract(xml_files, "data_(\\d{8}_\\d{6})\\.xml", group = 1))
 
-# Find all place nodes
-places <- xml_find_all(xml_doc, "//place")
-
-
-bikes_data <- list()
-
-# Loop through each place
-for (place in places) {
-  place_uid <- xml_attr(place, "uid")
-  place_name <- xml_attr(place, "name")
-  place_lat <- xml_attr(place, "lat")
-  place_lng <- xml_attr(place, "lng")
-  place_number <- xml_attr(place, "number")
-  n_bikes <- xml_attr(place, "bikes")
-  booked_bikes <- xml_attr(place, "booked_bikes")
-  
-  bikes <- xml_find_all(place, "./bike")
-  
-  if (length(bikes) > 0) {
-    for (bike in bikes) {
-      bike_data <- list(
-        # Place information
-        place_uid = place_uid,
-        place_name = place_name,
-        place_lat = as.numeric(place_lat),
-        place_lng = as.numeric(place_lng),
-        place_number = place_number,
+# Parse XML files and generate dataframe
+parse_xml <- function(xml_doc) {
+  places <- xml_find_all(xml_doc, "//place")
+  bikes_data <- list()
+  # Iterate through all places elements
+  for (place in places) {
+    place_uid <- xml_attr(place, "uid")
+    place_name <- xml_attr(place, "name")
+    place_lat <- xml_attr(place, "lat")
+    place_lng <- xml_attr(place, "lng")
+    place_number <- xml_attr(place, "number")
+    n_bikes <- xml_attr(place, "bikes")
+    booked_bikes <- xml_attr(place, "booked_bikes")
+    
+    bikes <- xml_find_all(place, "./bike")
+    
+    if (length(bikes) > 0) {
+      for (bike in bikes) {
+        bike_data <- list(
+          # Place information
+          place_uid = place_uid,
+          place_name = place_name,
+          place_lat = as.numeric(place_lat),
+          place_lng = as.numeric(place_lng),
+          place_number = place_number,
+          
+          # Bike information
+          bike_number = xml_attr(bike, "number"),
+          bike_type = xml_attr(bike, "bike_type"),
+          lock_types = xml_attr(bike, "lock_types"),
+          active = xml_attr(bike, "active"),
+          state = xml_attr(bike, "state"),
+          electric_lock = xml_attr(bike, "electric_lock"),
+          boardcomputer = xml_attr(bike, "boardcomputer"),
+          pedelec_battery = xml_attr(bike, "pedelec_battery"),
+          booked = booked_bikes == n_bikes
+        )
         
-        # Bike information
-        bike_number = xml_attr(bike, "number"),
-        bike_type = xml_attr(bike, "bike_type"),
-        lock_types = xml_attr(bike, "lock_types"),
-        active = xml_attr(bike, "active"),
-        state = xml_attr(bike, "state"),
-        electric_lock = xml_attr(bike, "electric_lock"),
-        boardcomputer = xml_attr(bike, "boardcomputer"),
-        pedelec_battery = xml_attr(bike, "pedelec_battery"),
-        booked = booked_bikes == n_bikes
-      )
-      
-      bikes_data[[length(bikes_data) + 1]] <- bike_data
+        bikes_data[[length(bikes_data) + 1]] <- bike_data
+      }
     }
   }
+  bind_rows(bikes_data)
 }
 
-df_bikes <- bind_rows(bikes_data)
+df_bikes <- map_dfr(xml_docs, parse_xml, .id = "date_time")
+write_rds(df_bikes, here("data", "26-transport", "df_bikes.rds"), compress = "gz")
